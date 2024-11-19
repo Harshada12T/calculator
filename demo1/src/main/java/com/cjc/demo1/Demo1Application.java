b26cca17fd1861193400317d4984574a2b0b2ea5
DELL-PC <DELL-PC@PC>
2024-11-19 16:28:48 +0530

    add variable

--- demo1/src/main/java/com/cjc/demo1/Demo1Application.java
+++ demo1/src/main/java/com/cjc/demo1/Demo1Application.java
@@ -9,6 +9,7 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 		System.out.println("This is jar project");
+		System.out.println("this is war project");
 	}
 
 }
